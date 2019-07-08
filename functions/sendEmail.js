8eef05e76ad210fab75cea24bbdc0704eaeba0f6
Akeem Balogun <balogunbiola101@gmail.com>
2019-07-08 17:03:41 +0100

    netlify-email-verification

---
+++ functions/sendEmail.js
@@ -0,0 +1,42 @@
+const result = require('dotenv').config()
+const nodemailer = require('nodemailer');
+const user = result.parsed.user
+const pass = result.parsed.pass
+
+
+export async function handler(event, context, callback){
+    const min = 10000;
+    const max = 99999;
+    const num = Math.floor(Math.random() * (max - min + 1)) + min;
+    const transport = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
+      auth: {
+        user,
+        pass
+      }
+    });
+
+    const { email }  = JSON.parse(event.body) 
+    let mailOptions = {
+      from: 'no-reply',
+      to: email, 
+      subject: 'Verification Code',
+      text: `${num}`,
+  };
+
+try{
+  let value = await transport.sendMail(mailOptions);
+  return {
+    statusCode: 200,
+    body: 'Success'
+  }
+}catch(err){
+  return {
+    statusCode: 400,
+    body: 'Failure'
+  };
+}
+
+}
